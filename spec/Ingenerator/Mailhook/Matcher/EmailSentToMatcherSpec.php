358e8f9eee816a4ae5f16e2054c508ad5cbb6532
Andrew Coulton <andrew@ingenerator.com>
2014-09-01 12:49:29 +0100

    Add EmailSentToMatcher

---
+++ spec/Ingenerator/Mailhook/Matcher/EmailSentToMatcherSpec.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Defines EmailSentToMatcherSpec - specifications for Ingenerator\Mailhook\Matcher\EmailSentToMatcher
+ *
+ * @copyright  2014 inGenerator Ltd
+ * @licence    BSD
+ */
+
+namespace spec\Ingenerator\Mailhook\Matcher;
+
+use Prophecy\Argument;
+use spec\ObjectBehavior;
+
+/**
+ *
+ * @see Ingenerator\Mailhook\Matcher\EmailSentToMatcher
+ */
+class EmailSentToMatcherSpec extends EmailMatcherBehaviour
+{
+    /**
+     * Use $this->subject to get proper type hinting for the subject class
+     * @var \Ingenerator\Mailhook\Matcher\EmailSentToMatcher
+     */
+	protected $subject;
+
+	function let()
+	{
+		$this->beConstructedWith('test@ingenerator.com');
+	}
+
+	function it_is_initializable()
+    {
+		$this->subject->shouldHaveType('Ingenerator\Mailhook\Matcher\EmailSentToMatcher');
+	}
+
+	/**
+	 * @param \Ingenerator\Mailhook\Email $email
+	 */
+	function it_does_not_match_email_sent_to_someone_else($email)
+	{
+		$this->subject->beConstructedWith('test@ingenerator.com');
+		$email->getTo()->willReturn('someoneelse@ingenerator.com');
+		$this->subject->matches($email)->shouldBe(FALSE);
+	}
+
+	/**
+	 * @param \Ingenerator\Mailhook\Email $email
+	 */
+	function it_matches_email_sent_to_recipient($email)
+	{
+		$this->subject->beConstructedWith('test@ingenerator.com');
+		$email->getTo()->willReturn('test@ingenerator.com');
+		$this->subject->matches($email)->shouldBe(TRUE);
+	}
+
+	function it_describes_with_requested_recipient()
+	{
+		$this->subject->beConstructedWith('test@ingenerator.com');
+		$this->subject->__toString()->shouldBe('To "test@ingenerator.com"');
+	}
+
+}
---
+++ src/Ingenerator/Mailhook/Matcher/EmailSentToMatcher.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Defines Ingenerator\Mailhook\Matcher\EmailSentToMatcher
+ *
+ * @copyright  2014 inGenerator Ltd
+ * @licence    BSD
+ */
+
+
+namespace Ingenerator\Mailhook\Matcher;
+
+use Ingenerator\Mailhook\Email;
+use Ingenerator\Mailhook\EmailMatcher;
+
+/**
+ * Matches the recipient email address
+ *
+ * @package Ingenerator\Mailhook\Matcher
+ * @see     spec\Ingenerator\Mailhook\Matcher\EmailSentToMatcherSpec
+ */
+class EmailSentToMatcher implements EmailMatcher {
+
+	/**
+	 * @var string
+	 */
+	protected  $recipient;
+
+	/**
+	 * @param string $recipient
+	 */
+	public function __construct($recipient)
+	{
+		$this->recipient = $recipient;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __toString()
+	{
+		return sprintf('To "%s"', $this->recipient);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function matches(Email $email)
+	{
+		return ($email->getTo() === $this->recipient);
+	}
+}
